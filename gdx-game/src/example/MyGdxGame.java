f028e3d6331e0b64ce7fce03a7a248b4d94783f9
jwright159 <iamjwright159@gmail.com>
2019-01-16 14:40:46 -0500

    Initial commit

---
+++ gdx-game/src/example/MyGdxGame.java
@@ -0,0 +1,22 @@
+package example;
+
+import com.badlogic.gdx.*;
+
+public class MyGdxGame extends Game{
+	public static MyGdxGame game;
+	
+	@Override
+	public void create(){
+		game = this;
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		//Tenebrae.debug("ROOT FILEPATH: "+Gdx.files.external("WrightWay/Tenebre 9/pak/").path());
+		setScreen(new ScreenExtendingWScreen());
+	}
+
+	@Override
+	public void dispose(){
+		//Tenebrae.debug("Disposing! :D");
+		super.dispose();
+		getScreen().dispose();
+	}
+}
